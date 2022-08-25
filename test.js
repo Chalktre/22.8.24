037b33ee8bc8456f02d536a504daa5370857f490
Chalktre <107284592+Chalktre@users.noreply.github.com>
2022-08-25 09:00:10 +0800

    Add files via upload

---
+++ test.js
@@ -0,0 +1,9 @@
+// var num = 0x20;
+// console.log(num);
+// console.log(Number.MAX_VALUE);
+// var str = '四个字符';
+// console.log(str.length) ;
+var num1 = parseInt(prompt('input a number'));
+var num2 = parseInt(prompt('input another number'));
+
+alert('the sum is ' + (num1 + num2));
